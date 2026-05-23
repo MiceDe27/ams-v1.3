9cb9dcb9211e237892694165b8fd2225c9a56ae6
Mice <180280929+MiceDe27@users.noreply.github.com>
2026-05-23 19:02:46 +0800

    Initial commit

--- models/accountModel.js
+++ models/accountModel.js
@@ -4,7 +4,7 @@ const AccountSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email:{type: String, required:true},
     password: {type: String, required:true},
-    website: {type: String, required: true}
+    website: {type: String, required: false}
 },
 {timestamps:true}
 );
